3f993a36b16a59f677a20fc198d04c2341536c40
Lachlan <Lachlan-Git@outlook.com>
2023-08-21 16:27:07 +1000

    Add basic functionality for PostgreSQL connection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
+import psycopg2
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
-
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
@@ -56,7 +56,13 @@ def home():
 
 @app.route("/issues")
 def issues():
-    return render_template("issues.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    conn = psycopg2.connect(database=env.get("DATABASE"), user=env.get("USER"), password=env.get("PASSWORD"), host=env.get("HOST"), port=env.get("PORT"))
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM employees''')
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("issues.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4), data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
