bbd023fcb6b0efa22f304229e694243ccba9b04c
Lachlan <Lachlan-Git@outlook.com>
2023-08-28 16:45:54 +1000

    Add issue sorting when clicking on table headers

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from os import environ as env
 from urllib.parse import quote_plus, urlencode
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, redirect, render_template, session, url_for, request
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -54,16 +54,19 @@ def logout():
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
-@app.route("/issues")
+@app.route("/issues", methods=['POST'])
 def issues():
     conn = psycopg2.connect(database=env.get("DATABASE"), user=env.get("USER"), password=env.get("PASSWORD"), host=env.get("HOST"), port=env.get("PORT"))
     cur = conn.cursor()
-    cur.execute(''' SELECT issues.id, projects.name, issues.title, issues.status, issues.priority, issues.date, issues.target_date, E1.name,
-                        E2.name
-                    FROM issues, projects, employees E1, employees E2
-                    WHERE issues.project_id = projects.id
-                    AND issues.developer_id = E1.id
-                    AND issues.tester_id = E2.id''')
+    query = ''' SELECT issues.id, projects.name, issues.title, issues.status, issues.priority, issues.date, issues.target_date, E1.name, E2.name
+                FROM issues, projects, employees E1, employees E2
+                WHERE issues.project_id = projects.id
+                AND issues.developer_id = E1.id
+                AND issues.tester_id = E2.id
+                ORDER BY {0} '''
+    orderBy = request.form.get("textField")
+    query = query.format(orderBy)
+    cur.execute(query)
     data = cur.fetchall()
     cur.close()
     conn.close()
